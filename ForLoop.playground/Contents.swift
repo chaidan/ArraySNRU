a0854d22ea697f331183652b7fb339bbe3a4c065
student14 <chaidan@msn.com>
2017-06-14 13:39:32 +0700

    Xcode for loop

---
+++ ForLoop.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// implicit 
+let intNumber = [11,22,33,44,55]
+var intIndex = 0
+// For loop basic
+for intArray in intNumber{
+    print("inArray==>(\(intIndex))=(\(intArray))")
+    intIndex += 1
+}
+let strArrayName = ["AAA","BBB","CCC","DDD"]
+for strMyName in strArrayName {
+    print("strMyName==>(\(strMyName))")
+
+
+}
